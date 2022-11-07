7e8b6961185b219e8db96205863f25a6ae26a374
Kevin <kevinmscollins@gmail.com>
2022-11-07 10:45:48 +0000

    Menu classes set up

---
+++ GP/src/Course.java
@@ -0,0 +1,35 @@
+package GP.src;
+
+import java.util.ArrayList;
+
+public class Course {
+
+    private String name ;
+    private ArrayList<MenuItem> items = new ArrayList();
+
+    public Course(String name){
+        this.name = name;
+    }
+
+    public ArrayList<MenuItem> getItems() { return items; }
+    public String getName() { return name; }
+
+    public void addMenuItem(MenuItem food){ items.add(food);}
+    public void removeMenuItem(MenuItem food) {items.remove(food);}
+
+    public void showMenu() {
+        System.out.println(getName());
+        for (MenuItem menuItem : items) {
+            System.out.println(menuItem);
+        }
+    }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder("\n***MENU***\n");
+        for (MenuItem item : items) {
+            str.append(item.toString()).append("\n***\n");
+        }
+        return str.toString();
+    }
+}
+
