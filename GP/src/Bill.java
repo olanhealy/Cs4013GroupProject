746e1147abc533873412a0a8724ace16361f34da
Sean Caplis <66325420+silpac-12@users.noreply.github.com>
2022-11-09 18:57:13 +0000

    Add files via upload
    Bill for specified tables order, calculates the total price and allows for payment

---
+++ GP/src/Bill.java
@@ -0,0 +1,56 @@
+import java.time.LocalDate;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+public class Bill{
+
+	private MenuItem item;
+	
+	private Table table;
+	
+	private String paymentMethod;
+	
+	private double total;
+	
+	public Bill(Table table, String paymentMethod) {
+		this.table = table;
+		this.paymentMethod = paymentMethod;
+		for(int i = 0; i < Course.getItems().size(); i++) {
+			item = Course.getItems().get(i);
+			total = total + item.getPrice();
+		}
+	}
+	
+	public double getTotal() {
+		return total;
+	}
+	
+	public String toString() {
+		return table.toString() +" \n"+ paymentMethod + "\n $" + total;
+	}
+	
+	public double Pay(double tender) {
+		if(paymentMethod == "Cash") {
+			total = total - tender;
+			if(total < 0) {
+				//Customer change
+				total = -total;
+				System.out.println(" Customer change " + total);
+				return total;
+				
+				}else if(total == 0) {
+					return 0;
+				}else {
+					System.out.println(" Insufficent Cash");
+				}
+			}
+		//Card Payment
+		if(paymentMethod == "Card" && total < tender){
+			System.out.println("\n Payment complete");
+		}else if(paymentMethod == "Card" && total > tender){
+			System.out.println("\n Card declined");
+		}
+		return 0;
+	}
+	
+}
+
